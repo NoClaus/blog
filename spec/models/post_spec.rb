6faa21bd68bc2511214dc19494ee27f08f06a71b
NoClaus <support@marketer.consulting>
2021-08-11 18:31:11 +0000

    Some fixies in Rspec config and Post model test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   
-  def build_and_validate_article(*args)
+  def build_and_validate_post(*args)
     post = build(:post, *args)
     post.validate!
     post
   end
   
-  let(:user) { create(:user) }
-  let!(:post) { create(:post, user: user) }
+  # let(:user) { create(:user) }
+  # let!(:post) { create(:post, user: user) }
 
   describe "validations" do
     it { is_expected.to validate_attached_of(:thumbnail) }
     it { is_expected.to validate_content_type_of(:thumbnail).allowing('image/png', 'image/jpg', 'image/jpeg') }
-    it { is_expected.to validate_size_of(:thumbnail).less_than(50.kilobytes) }
+    it { is_expected.to validate_size_of(:thumbnail).less_than(150.kilobytes) }
   end
 end
